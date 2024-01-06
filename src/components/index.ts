1be18e6c8862b4dae3768afab05e8f9c3b041cc2
oscar <oscar@avena.io>
2024-01-06 15:18:36 -0600

    feat: structure correction text page

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,8 @@
 export { default as DashboardLayout } from "./layouts/DashboardLayout";
 export * from "./ChatBubble";
 export * from "./SidebarMenuItem";
+export * from "./TextMessageBox";
+export * from "./TextMessageBoxSelect";
+export * from "./TextMessageBoxFile";
+export * from "./loaders/TypingLoader";
+export * from "./layouts/DashboardLayout";
